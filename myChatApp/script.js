b2ac8bc45317e6618b9358f3c6c4d932db3ae266
Ana <andrijanic.ana93@gmail.com>
2023-11-28 23:35:40 +0100

    Add files via upload

---
+++ myChatApp/script.js
@@ -0,0 +1,184 @@
+const CLIENT_ID = "7ChfIO2RSzySrt5p";
+
+const drone = new ScaleDrone(CLIENT_ID, {
+  data: {
+    name: getRandomName(),
+    color: getRandomColor(),
+  },
+});
+
+let members = [];
+
+drone.on("open", (error) => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log("Successfully connected to Scaledrone");
+
+  const room = drone.subscribe("observable-room");
+  room.on("open", (error) => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log("Successfully joined room");
+  });
+
+  room.on("members", (m) => {
+    members = m;
+    updateMembersDOM();
+  });
+
+  room.on("member_join", (member) => {
+    members.push(member);
+    updateMembersDOM();
+  });
+
+  room.on("member_leave", ({ id }) => {
+    const index = members.findIndex((member) => member.id === id);
+    members.splice(index, 1);
+    updateMembersDOM();
+  });
+
+  room.on("data", (text, member) => {
+    if (member) {
+      addMessageToListDOM(text, member);
+    } else {
+    }
+  });
+});
+
+drone.on("close", (event) => {
+  console.log("Connection was closed", event);
+});
+
+drone.on("error", (error) => {
+  console.error(error);
+});
+
+function getRandomName() {
+  const adjs = [
+    "panicky",
+    "subsequent",
+    "painful",
+    "special",
+    "picayune",
+    "gruesome",
+    "gainful",
+    "actually",
+    "superb",
+    "wet",
+    "immediate",
+    "complete",
+    "elegant",
+    "undesirable",
+    "helpful",
+    "statuesque",
+    "scientific",
+    "bright",
+    "plucky",
+    "ratty",
+    "spiky",
+    "subdued",
+    "fretful",
+    "unkempt",
+    "woebegone",
+    "level",
+    "sassy",
+    "shocking",
+  ];
+  const nouns = [
+    "poetry",
+    "literature",
+    "reflection",
+    "depth",
+    "customer",
+    "application",
+    "girlfriend",
+    "grandmother",
+    "professor",
+    "queen",
+    "painting",
+    "assistance",
+    "wife",
+    "speaker",
+    "charity",
+    "news",
+    "response",
+    "family",
+    "month",
+    "thought",
+    "friendship",
+    "conversation",
+    "night",
+    "imagination",
+    "affair",
+    "tooth",
+    "poet",
+  ];
+  return (
+    adjs[Math.floor(Math.random() * adjs.length)] +
+    "_" +
+    nouns[Math.floor(Math.random() * nouns.length)]
+  );
+}
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+}
+
+const DOM = {
+  messages: document.querySelector(".messages"),
+  input: document.querySelector(".message-form__input"),
+  form: document.querySelector(".message-form"),
+};
+
+DOM.form.addEventListener("submit", sendMessage);
+
+function sendMessage() {
+  const value = DOM.input.value;
+  if (value === "") {
+    return;
+  }
+  DOM.input.value = "";
+  drone.publish({
+    room: "observable-room",
+    message: value,
+  });
+}
+
+function createMemberElement(member) {
+  const { name, color } = member.clientData;
+  const el = document.createElement("div");
+  el.appendChild(document.createTextNode(name));
+  el.className = "member";
+  el.style.color = color;
+  return el;
+}
+
+function createMessageElement(text, member) {
+  const el = document.createElement("div");
+  el.appendChild(createMemberElement(member));
+  el.appendChild(document.createTextNode(text));
+  el.className = "message";
+  return el;
+}
+
+function addMessageToListDOM(text, member) {
+  const el = DOM.messages;
+  const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+  el.appendChild(createMessageElement(text, member));
+  if (wasTop) {
+    el.scrollTop = el.scrollHeight - el.clientHeight;
+  }
+}
+
+function updateDateTime() {
+  // create a new `Date` object
+  const now = new Date();
+
+  // get the current date and time as a string
+  const currentDateTime = now.toLocaleString();
+
+  // update the `textContent` property of the `span` element with the `id` of `datetime`
+  document.querySelector("#datetime").textContent = currentDateTime;
+}
